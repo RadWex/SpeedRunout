e74611ab0960ab2a9c4da1238a678a21609ef5f1
Damian <rutkowskid98@gmail.com>
2021-02-03 11:24:05 +0100

    Simple test

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ pipeline {
         }
         stage('Test') {
             steps {
-              sh '. env/bin/activate && python ./manage.py runtests'
+              sh '. env/bin/activate && python ./manage.py test'
             }
         }
     }
--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        self.text = "Hello world"
+
+    def test_hello_world(self):
+        """Simple test"""
+        self.assertEqual(self.text, "Hello world")
